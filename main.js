7a4841fa76a2ec23547f96eb3bf2de579a294ef5
김은혜 <jailke93@gmail.com>
2021-02-15 15:50:15 +0900

    bugGame: menu-start

--- main.js
+++ main.js
@@ -2,10 +2,20 @@ const playBtn = document.querySelector(".game__play");
 const playIcon = document.querySelector(".play__icon");
 const timer = document.querySelector(".game__timer");
 const score = document.querySelector(".game__score");
+const field = document.querySelector(".game__field");
 
-let timeDuration = 20;
+const bug = document.querySelector("bug");
+const carrot = document.querySelector("carrot");
 
-// menu
+const popUp = document.querySelector(".pop-up");
+
+let BUG_NUM = 5;
+let CARROT_NUM = 5;
+let ITEM_SIZE = 80;
+
+let timeDuration = 10;
+
+// menu : start
 // 1. play button click
 playBtn.addEventListener("click", () => {
   // 1-1. play -> stop button
@@ -13,8 +23,11 @@ playBtn.addEventListener("click", () => {
   playIcon.classList.add("fa-stop-circle");
   // 1-2. show timer & score
   showTimerAndScore();
+  // 1-3. show bug, carrot on field
+  addItem("bug", BUG_NUM, "img/bug.png");
+  addItem("carrot", CARROT_NUM, "img/carrot.png");
 });
-// 1-1. play -> stop button
+
 // 1-2. show timer & score
 function showTimerAndScore() {
   timer.style.visibility = "visible";
@@ -23,14 +36,53 @@ function showTimerAndScore() {
 }
 // 1-2-1. start timer
 function startTimer() {
-  const remainTime = timeDuration;
-  timer.innerText = `00:${remainTime}`;
-  setInterval(goTime, 1000);
+  let remainTime = timeDuration;
+  updateTimerText(remainTime);
+  setInterval(() => {
+    if (remainTime <= 0) {
+      clearInterval(timer);
+      return;
+    }
+    updateTimerText(--remainTime);
+  }, 1000);
 }
 
-function goTime() {
-  let remainTime = timeDuration;
-  remainTime = remainTime - 1;
+function updateTimerText(time) {
+  timer.innerText = `0:${time}`;
 }
 
 // 1-3. show bug, carrot on field
+function addItem(item, num, src) {
+  const rect = field.getBoundingClientRect();
+  x1 = 0;
+  x2 = rect.width - ITEM_SIZE;
+  y1 = 0;
+  y2 = rect.height - ITEM_SIZE;
+  for (let i = 0; i < num; i++) {
+    item = document.createElement("img");
+    item.setAttribute("class", item);
+    item.setAttribute("src", src);
+    item.style.position = "absolute";
+    item.style.top = `${randomNumber(y1, y2)}px`;
+    item.style.left = `${randomNumber(x1, x2)}px`;
+    field.appendChild(item);
+  }
+}
+
+function randomNumber(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+// field: click
+bug.addEventListener("click", () => {
+  stopGame();
+});
+
+carrot.addEventListener("click", () => {
+  gainScore();
+});
+
+function stopGame() {
+  clearInterval(timer);
+  popUp.classList.remove("pop-up--hide");
+}
