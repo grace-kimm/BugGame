a86ebbc9c4971dd5d43c817212ce1dd74b928858
김은혜 <jailke93@gmail.com>
2021-02-15 17:35:28 +0900

    bugGame: start menu

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const playBtn = document.querySelector(".game__play");
 const playIcon = document.querySelector(".play__icon");
-const timer = document.querySelector(".game__timer");
-const score = document.querySelector(".game__score");
+const timerBoard = document.querySelector(".game__timer");
+const scoreBoard = document.querySelector(".game__score");
 const field = document.querySelector(".game__field");
 
-const bug = document.querySelector("bug");
-const carrot = document.querySelector("carrot");
+const bug = document.querySelector(".bug");
+const carrot = document.querySelector(".carrot");
 
 const popUp = document.querySelector(".pop-up");
 
@@ -13,14 +13,19 @@ let BUG_NUM = 5;
 let CARROT_NUM = 5;
 let ITEM_SIZE = 80;
 
+let started = false;
 let timeDuration = 10;
+let score = 0;
+let timer = undefined;
 
 // menu : start
 // 1. play button click
 playBtn.addEventListener("click", () => {
   // 1-1. play -> stop button
+  started = true;
   playIcon.classList.remove("fa-play-circle");
   playIcon.classList.add("fa-stop-circle");
+
   // 1-2. show timer & score
   showTimerAndScore();
   // 1-3. show bug, carrot on field
@@ -30,8 +35,8 @@ playBtn.addEventListener("click", () => {
 
 // 1-2. show timer & score
 function showTimerAndScore() {
-  timer.style.visibility = "visible";
-  score.style.visibility = "visible";
+  timerBoard.style.visibility = "visible";
+  scoreBoard.style.visibility = "visible";
   startTimer();
 }
 // 1-2-1. start timer
@@ -48,11 +53,11 @@ function startTimer() {
 }
 
 function updateTimerText(time) {
-  timer.innerText = `0:${time}`;
+  timerBoard.innerText = `0:${time}`;
 }
 
 // 1-3. show bug, carrot on field
-function addItem(item, num, src) {
+function addItem(itemName, num, src) {
   const rect = field.getBoundingClientRect();
   x1 = 0;
   x2 = rect.width - ITEM_SIZE;
@@ -60,7 +65,7 @@ function addItem(item, num, src) {
   y2 = rect.height - ITEM_SIZE;
   for (let i = 0; i < num; i++) {
     item = document.createElement("img");
-    item.setAttribute("class", item);
+    item.setAttribute("class", itemName);
     item.setAttribute("src", src);
     item.style.position = "absolute";
     item.style.top = `${randomNumber(y1, y2)}px`;
@@ -73,16 +78,28 @@ function randomNumber(min, max) {
   return Math.random() * (max - min) + min;
 }
 
-// field: click
-bug.addEventListener("click", () => {
-  stopGame();
-});
+// menu: finish
 
-carrot.addEventListener("click", () => {
-  gainScore();
+// field: click
+field.addEventListener("click", (event) => {
+  const target = event.target;
+  if (target.matches(".bug")) {
+    stopGame();
+  } else if (target.matches(".carrot")) {
+    gainScore();
+  }
 });
 
 function stopGame() {
   clearInterval(timer);
   popUp.classList.remove("pop-up--hide");
+}
+
+function gainScore() {
+  score++;
+  updateScore();
+}
+
+function updateScore() {
+  scoreBoard.innerText = `${score}`;
 }
